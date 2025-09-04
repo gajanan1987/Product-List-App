acbb7ad7d85e0632cac20cd036e4fffaf2d277c1
gajanan1987 <nehete.gaju@gmail.com>
2025-09-04 18:34:57 +0530

    not found route add

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ export default function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/product-list" element={<ProductList />} />
+      <Route path="*" element={<h1>Page Not Found</h1>} />
     </Routes>
   );
 }
